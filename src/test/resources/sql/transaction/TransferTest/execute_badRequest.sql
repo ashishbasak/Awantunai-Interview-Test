5da74cac45be7b88bee7328698fc6c404ce18bb2
Komang Arinanda <komang.arinanda@gdn-commerce.com>
2023-05-08 17:34:42 +0700

    remove test file

--- src/test/resources/sql/transaction/TransferTest/execute_badRequest.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tbl_users(username, id)
-VALUES ('username', 1)
-;
-
-INSERT INTO tbl_user_logins(token, user_id, id)
-VALUES ('user-token', 1, 1);
--- src/test/resources/sql/transaction/TransferTest/execute_notFound_destinationUserNotFound.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tbl_users(username, id)
-VALUES ('username', 1)
-;
-
-INSERT INTO tbl_user_logins(token, user_id, id)
-VALUES ('user-token', 1, 1);
-
-INSERT INTO tbl_balances(amount, debit_amount, user_id, id)
-VALUES (10000, 0, 1, 1);
